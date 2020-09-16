5ea00e12c9c30149fdc6977c795840f88212226b
Raymond Thompson <68765572+ray1842@users.noreply.github.com>
2020-09-15 21:04:15 -0400

    Updates

---
+++ precache-manifest.6129c96bd4dbaa93f6484e82fdab7698.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f47920111cf71af6a6bc27eaeb0948d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2512ad73405e58fdd815",
+    "url": "/portfolio/static/css/main.5841f9ef.chunk.css"
+  },
+  {
+    "revision": "d77b649ae2e739b5c5b4",
+    "url": "/portfolio/static/js/2.19fb4646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.19fb4646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2512ad73405e58fdd815",
+    "url": "/portfolio/static/js/main.29307df0.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ac631e5cd939b250fd361a42bc88f4f7",
+    "url": "/portfolio/static/media/arrow_down.ac631e5c.svg"
+  },
+  {
+    "revision": "f30eed10d276149ebd8bdefb67d246c6",
+    "url": "/portfolio/static/media/contact.f30eed10.svg"
+  },
+  {
+    "revision": "3f2d81fb53a28c4892007d67f5137e2d",
+    "url": "/portfolio/static/media/home.3f2d81fb.svg"
+  },
+  {
+    "revision": "4b7156791ee09be5d834d28fe017ec10",
+    "url": "/portfolio/static/media/html5.4b715679.svg"
+  },
+  {
+    "revision": "bbc5738c1aefe99a0df9e0fe479ba1a0",
+    "url": "/portfolio/static/media/javascript.bbc5738c.svg"
+  },
+  {
+    "revision": "f88139ef1090defe9463b2c372c47711",
+    "url": "/portfolio/static/media/leftarrow.f88139ef.svg"
+  },
+  {
+    "revision": "d887870045b080ffc114ac72464d19ae",
+    "url": "/portfolio/static/media/react.d8878700.svg"
+  },
+  {
+    "revision": "cbc923a563d2d2962d844b3be01b9230",
+    "url": "/portfolio/static/media/trading.cbc923a5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0da11d69fbca1a6be6b699b2717e1562.js"
+  "/portfolio/precache-manifest.6129c96bd4dbaa93f6484e82fdab7698.js"
 );
 
 self.addEventListener('message', (event) => {
